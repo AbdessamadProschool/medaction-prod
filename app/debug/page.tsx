0c9ca07e5dbd96870b78bfef74942f4bd3afbca1
Medaction Deploy <deploy@medaction.local>
2026-02-10 15:31:18 +0100

    Add debug route to isolate Next.js runtime issues

---
+++ app/debug/page.tsx
@@ -0,0 +1,8 @@
+export default function DebugPage() {
+    return (
+        <div>
+            <h1>DEBUG OK</h1>
+            <p>Next.js version: {process.env.NEXT_RUNTIME}</p>
+        </div>
+    )
+}
